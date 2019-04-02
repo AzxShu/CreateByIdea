a5421f4126eac39ff138fc2f302e6247d4c863d0
AzxShu <409611251zsx@gmail.com>
2019-04-02 19:41:10 +0800

    from idea terminal by Ashu

--- src/testdemo01.java
+++ src/testdemo01.java
@@ -2,5 +2,6 @@
 public class testdemo01{
     public static void main(String[] args) {
         System.out.println("no 我是火车王 idea_c");
+        System.out.println("no hello i will reject you!");
     }
 }
