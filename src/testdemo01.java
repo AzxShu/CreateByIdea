629f9b2ac940ab733d444c7e93b2882e4cffcd6f
AzxShu <409611251zsx@gmail.com>
2019-04-02 20:13:18 +0800

    modifiedByIdea by Ashu

--- src/testdemo01.java
+++ src/testdemo01.java
@@ -3,5 +3,6 @@ public class testdemo01{
     public static void main(String[] args) {
         System.out.println("no 我是火车王 idea_c");
         System.out.println("no hello i will reject you!");
+        System.out.println("123");
     }
 }
